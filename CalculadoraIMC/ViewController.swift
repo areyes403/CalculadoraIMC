d24f538ab67340013c19d740020603a4551af64f
areyes403 <areyes403>
2022-03-02 10:01:12 -0600

    Se reparo todo en general y se agrego la primer view

--- CalculadoraIMC/ViewController.swift
+++ CalculadoraIMC/ViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lblPeso: UILabel!
+    @IBOutlet weak var lblAltura: UILabel!
     var cerebroCalculadora = CerebroCalculadora()
+    var peso:Double = 75.0
+    var altura:Double = 1.6
+    var imc:Double = 0.0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func slPeso(_ sender: UISlider) {
+        lblPeso.text="\(String(format: "%.2f", sender.value)) kg"
+        peso = Double(sender.value)
+        
+    }
+    /*
+    
+    @IBAction func slAltura(_ sender: Any) {
+        lblAltura.text="\(String(format: "%.2f", (sender as AnyObject).value)) cm"
+        
+    }
+ */
+    
+    @IBAction func slAltura(_ sender: UISlider) {
+        lblAltura.text="\(String(format: "%.2f", sender.value)) M"
+        altura = Double(sender.value)
+    }
+    
+    @IBAction func btnCalcular(_ sender: UIButton) {
+        print("calcular")
+    }
+    
 }
 
